c6e3eae451f37f40dd5446437b8fc4fa58b59ad7
Louis Macjob <louismacjob@gmail.com>
2023-11-15 15:04:29 +0100

    feat-functionalDate: updated the countdown date to be functional

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,41 @@ hamB.addEventListener('click', () => {
 
 let dropdownBtn = document.querySelector('.dropdownBtn');
 let dropdownMenu = document.querySelector('.content');
+const dayElement = document.querySelector('.day');
+const hourElement = document.querySelector('.hour');
+const minuteElement = document.querySelector('.minute');
+const secondElement = document.querySelector('.second');
+let days = 5;
+let hours = 24;
+let minutes = 60;
+let seconds = 60;
+function updateCountDown() {
+  seconds--;
 
-dropdownBtn.addEventListener('click', (e)=>{
-  dropdownMenu.classList.toggle('content-active')
-})
+  if (seconds <= 0) {
+    minutes--;
+    seconds = 60;
+  }
+  if (minutes <= 0) {
+    hours--;
+    minutes = 60;
+  }
+  if (hours <= 0) {
+    days--;
+    hours = 24;
+    1;
+  }
+
+  dayElement.innerHTML = days < 10 ? `0${days}` : days;
+  hourElement.innerHTML = hours < 10 ? `0${hours}` : hours;
+  minuteElement.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  secondElement.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+  // console.log(seconds);
+  // console.log(minutes);
+}
+
+setInterval(updateCountDown, 1000);
+
+dropdownBtn.addEventListener('click', (e) => {
+  dropdownMenu.classList.toggle('content-active');
+});
